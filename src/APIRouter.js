bc25bf77841c84d21ed75933ebc9ab9ed7b3a1dd
cyrmos <jy@t05.sg>
2022-11-24 17:11:32 +0800

    fix aggregate

--- src/APIRouter.js
+++ src/APIRouter.js
@@ -31,8 +31,8 @@ class APIRouter {
     let path = this._path;
     this._fastify.get(path, {}, this.routeHandler("routeList"));
     this._fastify.post(path, {}, this.routeHandler("routePost"));
-    this._fastify.post(path + "/aggregate", {}, this.routeHandler("routeAggregate"));
     this._fastify.get(path + "/:id", {}, this.routeHandler("routeGet"));
+    this._fastify.post(path + "/aggregate", {}, this.routeHandler("routeAggregate"));
     this._fastify.put(path + "/:id", {}, this.routeHandler("routePut"));
     this._fastify.patch(path + "/:id", {}, this.routeHandler("routePut"));
     this._fastify.delete(path + "/:id", {}, this.routeHandler("routeDelete"));

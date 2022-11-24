77156104c0ae5100d9aa7aec35d090a47a477688
cyrmos <jy@t05.sg>
2022-11-24 17:13:36 +0800

    fix aggregate

--- src/APIRouter.js
+++ src/APIRouter.js
@@ -31,8 +31,8 @@ class APIRouter {
     let path = this._path;
     this._fastify.get(path, {}, this.routeHandler("routeList"));
     this._fastify.post(path, {}, this.routeHandler("routePost"));
-    this._fastify.get(path + "/:id", {}, this.routeHandler("routeGet"));
     this._fastify.post(path + "/aggregate", {}, this.routeHandler("routeAggregate"));
+    this._fastify.get(path + "/:id", {}, this.routeHandler("routeGet"));
     this._fastify.put(path + "/:id", {}, this.routeHandler("routePut"));
     this._fastify.patch(path + "/:id", {}, this.routeHandler("routePut"));
     this._fastify.delete(path + "/:id", {}, this.routeHandler("routeDelete"));
@@ -263,9 +263,7 @@ class APIRouter {
       reply.callNotFound();
     } else {
       await this.populateIfNeeded(request, doc);
-      let ret = {
-        items: await this.docToAPIResponse(doc, request),
-      };
+      let ret = await this.docToAPIResponse(doc, request);
       reply.send(ret);
     }
   }
@@ -291,7 +289,9 @@ class APIRouter {
   }
 
   async routeAggregate(request, reply) {
-    let ret = await this._model.aggregate(request.body).exec();
+    let ret = {
+      items: await this._model.aggregate(request.body).exec(),
+    };
     reply.send(ret);
   }
 

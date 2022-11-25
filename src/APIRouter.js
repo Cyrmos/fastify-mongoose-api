bb60af3bf6860706c3d68e415e36b0018faa16c7
cyrmos <jy@t05.sg>
2022-11-25 13:45:10 +0800

    debug

--- src/APIRouter.js
+++ src/APIRouter.js
@@ -6,6 +6,7 @@ const parseAggregate = (aggregate) => {
       if (!Array.isArray(value)) parseAggregate(aggregate[key]);
       else aggregate[key] = value;
     } else {
+      console.log(value);
       const isDate = value.includes("$date");
       if (isDate) {
         const dateString = value.replace("$date", "");

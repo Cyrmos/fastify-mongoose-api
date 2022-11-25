3ddc4a12b853024e61d18e20668d0638bd288316
cyrmos <jy@t05.sg>
2022-11-25 14:40:06 +0800

    remove console log

--- src/APIRouter.js
+++ src/APIRouter.js
@@ -316,8 +316,6 @@ class APIRouter {
       pipeline.push(stage);
     }
 
-    console.log(JSON.stringify(pipeline, 0, 2));
-
     let ret = {
       items: await this._model.aggregate(pipeline).exec(),
     };

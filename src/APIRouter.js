fc17580044c57af709150fb3166b94e5e5e0589c
cyrmos <jy@t05.sg>
2022-11-25 14:46:09 +0800

    console log debugging

--- src/APIRouter.js
+++ src/APIRouter.js
@@ -316,6 +316,8 @@ class APIRouter {
       pipeline.push(stage);
     }
 
+    console.log(JSON.stringify(pipeline, 0, 2));
+
     let ret = {
       items: await this._model.aggregate(pipeline).exec(),
     };

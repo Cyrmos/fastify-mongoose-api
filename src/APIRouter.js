92e58c3843abbfce283994917b8f91ecb188fb4a
cyrmos <jy@t05.sg>
2022-11-29 13:32:27 +0800

    fix bulk delete

--- src/APIRouter.js
+++ src/APIRouter.js
@@ -35,8 +35,8 @@ class APIRouter {
     this._fastify.get(path + "/:id", {}, this.routeHandler("routeGet"));
     this._fastify.put(path + "/:id", {}, this.routeHandler("routePut"));
     this._fastify.patch(path + "/:id", {}, this.routeHandler("routePut"));
-    this._fastify.delete(path + "/bulk", {}, this.routeHandler("routeDelete"));
-    this._fastify.delete(path + "/:id", {}, this.routeHandler("routeBulkDelete"));
+    this._fastify.delete(path + "/bulk", {}, this.routeHandler("routeBulkDelete"));
+    this._fastify.delete(path + "/:id", {}, this.routeHandler("routeDelete"));
 
     /// check if there's apiSubRoutes method on the model
     if (this._model["apiSubRoutes"]) {
